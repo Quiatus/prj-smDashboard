409d2d35ae6698ceea1906b076aa6ce9fb84329d
Quiatus <Quiatus@gmail.com>
2023-11-29 14:43:55 +0100

    add lightmode toggle

--- app/js/script.js
+++ app/js/script.js
@@ -2,8 +2,29 @@ const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
 const dark = document.getElementById('dark');
 const light = document.getElementById('light');
 
-if (darkThemeMq.matches) {
-    dark.setAttribute('checked', '');
+if (localStorage.length > 0) {
+    document.querySelector('.body').classList.add(localStorage.getItem('colorMode'));
+    if (localStorage.getItem('colorMode') === 'dark') {
+        dark.setAttribute('checked', '');
+    } else {
+        light.setAttribute('checked', '');
+    }
 } else {
-    light.setAttribute('checked', '');
+    if (darkThemeMq.matches) {
+        dark.setAttribute('checked', '');
+    } else {
+        light.setAttribute('checked', '');
+    }
 }
+
+dark.addEventListener('click', function() {
+    document.querySelector('.body').classList.remove('light');
+    document.querySelector('.body').classList.add('dark');
+    localStorage.setItem('colorMode', 'dark');
+})
+
+light.addEventListener('click', function() {
+    document.querySelector('.body').classList.remove('dark');
+    document.querySelector('.body').classList.add('light');
+    localStorage.setItem('colorMode', 'light');
+})
